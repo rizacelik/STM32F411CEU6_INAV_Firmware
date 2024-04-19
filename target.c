2e2cee05e6d39d74b212d3d8b65cf56b056d8c81
Rıza Çelik <rizacelik@gmail.com>
2024-04-19 13:43:13 +0300

    Update target.c

--- target.c
+++ target.c
@@ -27,16 +27,30 @@ timerHardware_t timerHardware[] = {
     DEF_TIM(TIM9, CH2, PA3,   TIM_USE_PPM,   0, 0), // PPM IN
 
 
-    DEF_TIM(TIM3, CH1, PB4,  TIM_USE_MC_MOTOR | TIM_USE_FW_MOTOR,  0, 0), // S1  D(1,4,5)
-    DEF_TIM(TIM3, CH2, PB5,  TIM_USE_MC_MOTOR | TIM_USE_FW_MOTOR,  0, 0), // S2  D(1,5,5)
-    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO,  0, 0), // S3  D(1,0,2)
-    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO,  0, 0), // S4  D(1,3,2)
-    DEF_TIM(TIM2, CH1, PA5,  TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO,  0, 0), // S5  D(1,6,3)
-    DEF_TIM(TIM2, CH3, PB10, TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO,  0, 0), // S6  D(1,1,3)
-    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_MC_SERVO | TIM_USE_FW_SERVO,  0, 0), // S7
-    DEF_TIM(TIM5, CH2, PA1,  TIM_USE_MC_SERVO | TIM_USE_FW_SERVO,  0, 0), // S8
-    
-    DEF_TIM(TIM1, CH1, PA8,  TIM_USE_ANY,   0, 0), // 2812LED TIM_USE_LED   D(2,1,6)
+    DEF_TIM(TIM3, CH1, PB4,  TIM_USE_OUTPUT_AUTO,  0, 0), // S1  D(1,4,5)
+    DEF_TIM(TIM3, CH2, PB5,  TIM_USE_OUTPUT_AUTO,  0, 0), // S2  D(1,5,5)
+    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_OUTPUT_AUTO,  0, 0), // S3  D(1,0,2)
+    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_OUTPUT_AUTO,  0, 0), // S4  D(1,3,2)
+    DEF_TIM(TIM2, CH1, PA5,  TIM_USE_OUTPUT_AUTO,  0, 0), // S5  D(1,6,3) STM32F411CE SERVO 1 or MOTOR 5
+    DEF_TIM(TIM2, CH3, PB10, TIM_USE_OUTPUT_AUTO,  0, 0), // S6  D(1,1,3) STM32F411CE SERVO 2 or MOTOR 6
+
+#if defined(STM32F411CE_HEX)
+
+    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_OUTPUT_AUTO,  0, 0), // S7  D(1,2,6) SERVO 1
+    DEF_TIM(TIM5, CH2, PA1,  TIM_USE_OUTPUT_AUTO,  0, 0), // S8  D(1,4,6) SERVO 2
+
+#elif defined(STM32F411CE)
+
+    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_ANY,   0, 0), // Softserial2_TX  D(1,2,6)
+    DEF_TIM(TIM5, CH2, PA1,  TIM_USE_ANY,   0, 0), // Softserial2_RX  D(1,4,6)
+
+#endif
+
+    DEF_TIM(TIM1, CH2, PA9,  TIM_USE_ANY,  0, 0), // Softserial1_TX  D(1,0,2)
+    DEF_TIM(TIM1, CH3, PA10, TIM_USE_ANY,  0, 0), // Softserial1_RX  D(1,3,2)
+
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_BEEPER, 0, 0), // BEEPER PWM OPT
+    DEF_TIM(TIM1, CH1, PA8, TIM_USE_LED,    0, 0), // 2812LED TIM_USE_LED   D(2,1,6)
 
 };
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
